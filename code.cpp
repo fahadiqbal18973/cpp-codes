96527a79620336183e00fdd1a4b090f7f36fc0f4
Fahad Iqbal <fahadiqbal1901@gmail.com>
2026-06-04 01:40:38 +0530

    first commit

---
+++ code.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << (3 < 5) << endl; // true, outputs 1
+    cout << (3 > 5) << endl; // false, outputs 0
+    cout << (3 == 5) << endl; // false, outputs 0
+    cout << (3 != 5) << endl; // true, outputs 1
+return 0;
+}
+
+
