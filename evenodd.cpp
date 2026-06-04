04e115f7cae3151541241e7ba0a7e2f49d021279
Fahad Iqbal <fahadiqbal1901@gmail.com>
2026-06-04 17:35:24 +0530

    if-else practice

---
+++ evenodd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter any number:";
+    cin >> n;
+
+    if(n % 2 == 0){
+        cout << n << " is an even number "<< endl;
+    }else{
+        cout << n << " is an odd number "<< endl;
+    }
+return 0;
+}
+
+
+
---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ int main(){
+ float marks;
+
+ cout << "Enter your marks: ";
+ cin >> marks;
+ if(marks >= 33){
+     cout << "Congratulations! You have passed the exam." << endl;
+ }
+ else{
+     cout << "Sorry! You have failed the exam." << endl;
+ }
+ 
+return 0;
+}
+ 
