855e0d6a35c9ba0db2e19ac63d13a2fd7befc8e4
Fahad Iqbal <fahadiqbal1901@gmail.com>
2026-07-26 15:14:11 +0530

    Area of Square

---
+++ assignment.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cout << "Enter the Side of square in centimeters : ";
+  cin >> n;
+  int area = n * n;
+  cout << " Area of square is : " << area << " cm^2" << endl;
+  return 0;
+}
